01e6db6c2447fda8957db5093514a85ceda3f307
StijnDeWinter <stijndw@live.com>
2017-01-05 14:16:19 +0100

    Updates
    Added VCS file for Git

--- Aphasia/app/src/main/java/com/example/wolkje/aphasia/MainActivity.java
+++ Aphasia/app/src/main/java/com/example/wolkje/aphasia/MainActivity.java
@@ -1,11 +1,13 @@
-package com.example.gijs.mobieleaphasia;
+package com.example.wolkje.aphasia;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import com.example.wolkje.aphasia.R;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnTest1;
