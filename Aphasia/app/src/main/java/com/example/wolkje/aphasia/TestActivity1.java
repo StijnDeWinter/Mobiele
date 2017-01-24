435e2c4518676a43bc2286bd61d61f3e6a6ef325
gijsVerheyden <https://gijsver@bitbucket.org/gijsver/picross.git>
2017-01-24 11:02:51 +0100

    Recording of audio
    Recording of audi see testActivity1

--- Aphasia/app/src/main/java/com/example/wolkje/aphasia/TestActivity1.java
+++ Aphasia/app/src/main/java/com/example/wolkje/aphasia/TestActivity1.java
@@ -1,7 +1,13 @@
 package com.example.wolkje.aphasia;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -16,6 +22,7 @@ import com.example.wolkje.aphasia.model.Manager;
 import com.example.wolkje.aphasia.model.Questioning;
 import com.example.wolkje.aphasia.model.question.Question;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import static com.example.wolkje.aphasia.R.id.buttonNextQuestion;
@@ -36,13 +43,28 @@ public class TestActivity1 extends AppCompatActivity {
     private EditText answer;
     private Button submitAnswer;
 
+    /** Alles voor opnemen van geluid **/
+    private static final String LOG_TAG = "AudioRecord";
+    private static String mFileName = null;
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private RecordButton mRecordButton = null;
+    private MediaRecorder mRecorder = null;
+    // Requesting permission to RECORD_AUDIO
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test1);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         questionType1 = (TextView) findViewById(R.id.TextViewType1Question);
         questioning = new Questioning(getIntent().getExtras().getString("name"));
+        mRecordButton = (RecordButton) findViewById(R.id.buttonRecordAnswer);
+
+
         addListenerOnButton();
 
         currentPosition = 0;
@@ -58,6 +80,15 @@ public class TestActivity1 extends AppCompatActivity {
         /*pictures = (ListView) findViewById(R.id.image_list);
         pictures.setAdapter(new com.example.wolkje.aphasia.PictureListAdapter(TestActivity2.this, picturesID));*/
         //questions = new QuestionMaker(getApplicationContext()).generateQuestionList("type1", 10);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mRecorder != null) {
+            mRecorder.release();
+            mRecorder = null;
+        }
     }
 
     protected void onStart() {
@@ -107,4 +138,67 @@ public class TestActivity1 extends AppCompatActivity {
         finish();
     }
 
+    /** ALLES VOOR OPNEMEN VAN GELUID **/
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+    }
+
+    private void onRecord(boolean start) {
+        if (start) {
+            startRecording();
+        } else {
+            stopRecording();
+        }
+    }
+
+    private void startRecording() {
+        mRecorder  = new MediaRecorder();
+        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        mRecorder.setOutputFile(mFileName);
+        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+        try {
+            mRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+        }
+
+        mRecorder.start();
+    }
+
+    private void stopRecording() {
+        mRecorder.stop();
+        mRecorder.release();
+        mRecorder = null;
+    }
+
+    class RecordButton extends Button {
+        boolean mStartRecording = true;
+
+        OnClickListener clicker = new OnClickListener() {
+            public void onClick(View v) {
+                onRecord(mStartRecording);
+                if (mStartRecording) {
+                    setText("Stop recording");
+                } else {
+                    setText("Start recording");
+                }
+                mStartRecording = !mStartRecording;
+            }
+        };
+
+        public RecordButton(Context ctx) {
+            super(ctx);
+            setText("Start recording");
+            setOnClickListener(clicker);
+        }
+    }
 }
